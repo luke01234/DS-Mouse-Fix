9e51939b297e9f386c65c52fc479a2d2d4b07eb9
luke01234 <95252713+luke01234@users.noreply.github.com>
2023-07-30 01:39:29 -0500

    Added Scripts

---
+++ InputStruct.c
@@ -0,0 +1,15 @@
+/*
+Author: Luke Attard
+Date: 7/30/23
+purpose: This file contains a struct used to listen for user inputs and perform actions accordingly. 
+The struct contains: 
+  an int to determine if the button is down or not
+  a pointer to a game function
+  an int to determine what key this input listens for
+*/
+
+struct Input {
+  int isDown;
+  void (*gameFunction)();
+  int inpNum;
+};
